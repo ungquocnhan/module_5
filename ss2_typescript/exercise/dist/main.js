fcb58d4b945b54d1bf4f4999f759f75558c0d98f
ungquocnhan <nhan09bk@gmail.com>
2022-12-22 11:37:55 +0700

    module_5

--- ss2_typescript/exercise/dist/main.js
+++ ss2_typescript/exercise/dist/main.js
@@ -1,9 +1,6 @@
 function fibonacci(number) {
-    if (number == 0) {
-        return 0;
-    }
-    if (number == 1) {
-        return 1;
+    if (number <= 1) {
+        return number;
     }
     return fibonacci(number - 1) + fibonacci(number - 2);
 }
